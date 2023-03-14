f707307bdca018e38343b4e3b82ac1fbb7000093
naka520 <yashipipi0520@gmail.com>
2023-03-15 02:07:19 +0900

    "modify issue/7　ヘッダーボタンの追加"

--- hukyousns/pages/components/header.tsx
+++ hukyousns/pages/components/header.tsx
@@ -4,24 +4,61 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import LoginIcon from "@mui/icons-material/Login";
+import Router from "next/router";
+import HomeIcon from "@mui/icons-material/Home";
+import PublicIcon from "@mui/icons-material/Public";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 const Header = () => {
+  const handler = (path: string) => {
+    Router.push(path);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="inherit">
         <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit" component="div">
+            hukyouSNS
+          </Typography>
           <IconButton
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{ mr: 1, ml: 1 }}
+            onClick={() => handler("./MyPage")}
           >
-            <MenuIcon />
+            <HomeIcon />
           </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            hukyouSNS
-          </Typography>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 1 }}
+            onClick={() => handler("./home")}
+          >
+            <PublicIcon />
+          </IconButton>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 1 }}
+            onClick={() => handler("./signin")}
+          >
+            <LoginIcon />
+          </IconButton>
+
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 1 }}
+            onClick={() => handler("./signup")}
+          >
+            <AddCircleOutlineIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
