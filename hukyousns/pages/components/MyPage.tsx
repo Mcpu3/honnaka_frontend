350599146a6cce9e8d3b8aaa6dc470ccb600c971
naka520 <yashipipi0520@gmail.com>
2023-03-15 00:12:36 +0900

    modify issue#5  エラー修正1

--- hukyousns/pages/components/MyPage.tsx
+++ hukyousns/pages/components/MyPage.tsx
@@ -1,215 +1,9 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-// import Header from "./header";
-// import { Stack } from "@mui/system";
-
-// interface User {
-//   user_uuid: string;
-//   user_name: string;
-//   display_name: string;
-//   created_at: string;
-//   updated_at: string;
-// }
-
-// interface Articles {
-//   user_uuid: string;
-//   title: string;
-// }
-
-// const testData: Articles[] = [
-//   {
-//     user_uuid: "178b87a3-f21d-42de-963d-9f1a4ae25afb",
-//     title: "コザクラインコかわいい",
-//   },
-//   {
-//     user_uuid: "178b87a3-f21d-42de-963d-9f1a4ae25afb",
-//     title: " オウムはいいぞ",
-//   },
-// ];
-
-// const API_ENDPOINT = "https://honnaka-backend.azurewebsites.net/api/v1/me";
-
-// async function getUserInfo(): Promise<User> {
-//   const accessToken = localStorage.getItem("access_token");
-//   const response = await axios.get(API_ENDPOINT, {
-//     headers: {
-//       Authorization: `Bearer ${accessToken}`,
-//     },
-//   });
-//   return response.data;
-// }
-
-// const MyPage = () => {
-//   const [username, setUsername] = useState<string>("");
-//   const [myUser, setMyUser] = useState<User | null>(null);
-//   const [articles, setArticles] = useState<Articles[]>([]);
-
-//   useEffect(() => {
-//     const fetchMyPageData = async () => {
-//       try {
-//         const accessToken = localStorage.getItem("access_token");
-//         console.log(accessToken);
-//         if (!accessToken) {
-//           // アクセストークンがない場合は、ログイン画面にリダイレクト
-//           window.location.href = "./signin";
-//           return;
-//         }
-
-//         getUserInfo().then((user) => {
-//           setMyUser(user);
-//           console.log(myUser); // ユーザー情報をコンソールに出力する
-//         });
-
-//         const userInfo = await getUserInfo();
-//         console.log(userInfo);
-//       } catch (error) {
-//         console.error("Error:", error);
-//       }
-//     };
-//     fetchMyPageData();
-//   }, []);
-
-//   if (!myUser) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <Stack>
-//       <Header />
-//       <Stack sx={{ width: 400 }}>
-//         <div className="bg-gray-600">
-//           My Page
-//           <div>UserName : {myUser.user_name}</div>
-//         </div>
-//         <div>
-//           <h2>My Articles</h2>
-//           <ul>
-//             {testData.map((article) => (
-//               <li key={article.user_uuid}>
-//                 <h3>{article.title}</h3>
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       </Stack>
-//     </Stack>
-//   );
-// };
-
-// export default MyPage;
-
-//↑テストデータ表示用
-
-//↓本番環境提出用
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-// import Header from "./header";
-// import { Stack } from "@mui/system";
-
-// interface User {
-//   user_uuid: string;
-//   user_name: string;
-//   display_name: string;
-//   created_at: string;
-//   updated_at: string;
-// }
-
-// interface Articles {
-//   user_uuid: string;
-//   title: string;
-// }
-
-// const testData: Articles[] = [
-//   {
-//     user_uuid: "178b87a3-f21d-42de-963d-9f1a4ae25afb",
-//     title: "コザクラインコかわいい",
-//   },
-//   {
-//     user_uuid: "178b87a3-f21d-42de-963d-9f1a4ae25afb",
-//     title: " オウムはいいぞ",
-//   },
-// ];
-
-// const API_ENDPOINT =
-//   "https://honnaka-backend.azurewebsites.net/api/v1/me/posts";
-
-// async function getUserInfo(): Promise<User> {
-//   const accessToken = localStorage.getItem("access_token");
-//   const response = await axios.get(API_ENDPOINT, {
-//     headers: {
-//       Authorization: `Bearer ${accessToken}`,
-//     },
-//   });
-//   return response.data;
-// }
-
-// const MyPage = () => {
-//   const [username, setUsername] = useState<string>("");
-//   const [myUser, setMyUser] = useState<User | null>(null);
-//   const [articles, setArticles] = useState<Articles[]>([]);
-
-//   useEffect(() => {
-//     const fetchMyPageData = async () => {
-//       try {
-//         const accessToken = localStorage.getItem("access_token");
-
-//         if (!accessToken) {
-//           // アクセストークンがない場合は、ログイン画面にリダイレクト
-//           window.location.href = "./signin";
-//           return;
-//         }
-
-//         getUserInfo().then((user) => {
-//           setMyUser(user);
-//           //   console.log(myUser); // ユーザー情報をコンソールに出力する
-//         });
-
-//         console.log(myUser?.user_name);
-
-//         const userInfo = await getUserInfo();
-//         console.log(userInfo);
-//       } catch (error) {
-//         console.error("Error:", error);
-//       }
-//     };
-//     fetchMyPageData();
-//   }, []);
-
-//   if (!myUser) {
-//     return <div>Loading...</div>;
-//   }
-
-//   return (
-//     <Stack>
-//       <Header />
-//       <Stack sx={{ width: 400 }}>
-//         <div className="bg-gray-600">
-//           My Page
-//           <div>UserName : {myUser.user_name}</div>
-//         </div>
-//         <div>
-//           <h2>My Articles</h2>
-//           {/* <ul>
-//             {testData.map((article) => (
-//               <li key={article.user_uuid}>
-//                 <h3>{article.title}</h3>
-//               </li>
-//             ))}
-//           </ul> */}
-//         </div>
-//       </Stack>
-//     </Stack>
-//   );
-// };
-
-// export default MyPage;
-
-// ↓エラー回避
-
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "./header";
 import { Card, CardContent, Typography } from "@mui/material";
+import { ConnectedTvOutlined } from "@mui/icons-material";
+import { access } from "fs";
 
 interface User {
   user_uuid: "string";
@@ -218,6 +12,11 @@ interface User {
   created_at: "string";
   updated_at: "string";
 }
+
+interface PostsUuid {
+  posts_uuid: string[];
+}
+
 interface Post {
   post_uuid: string;
   user_uuid: string;
@@ -263,15 +62,22 @@ const styles = {
 const MyPage = () => {
   const [username, setUsername] = useState<string>("");
   const [myUser, setMyUser] = useState<User | null>(null);
-  const [post, setPost] = useState<Post | undefined>(undefined);
+  const [posts, setPosts] = useState<Post[] | undefined>(undefined);
   const [accessToken, setAccesstoken] = useState<string>("");
+  const [articles, setArticles] = useState<Post[]>([]);
+  const [leng, setLeng] = useState<number>(0);
+  const [postsUuid, setPostsUuid] = useState<PostsUuid | undefined>(undefined);
   useEffect(() => {
     let ignore = false;
 
-    const fetchPost = async () => {
+    const fetchPostsUuid = async () => {
       const accessToken = localStorage.getItem("access_token");
+      if (!accessToken) {
+        window.location.href = "./signin";
+        return;
+      }
       try {
-        const response = await axios.get<Post>(
+        const response = await axios.get<PostsUuid>(
           "https://honnaka-backend.azurewebsites.net/api/v1/me/posts",
           {
             headers: {
@@ -281,53 +87,64 @@ const MyPage = () => {
         );
 
         if (!ignore) {
-          setPost(response.data);
-          console.log("post:", response.data);
+          setPostsUuid(response.data);
+          console.log(response.data);
         }
       } catch (e) {
         console.log(`Exception: ${e}`);
       }
     };
-    if (!accessToken) return;
 
-    fetchPost();
+    fetchPostsUuid();
 
     return () => {
       ignore = true;
     };
-  }, [accessToken]);
+  }, []);
 
   useEffect(() => {
     let ignore = false;
 
-    const getPost = async () => {
+    const fetchPost = async (post_uuid: string): Promise<Post> => {
+      const response = await axios.get<Post>(
+        `https://honnaka-backend.azurewebsites.net/api/v1/post/${post_uuid}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      return response.data;
+    };
+
+    const fetchPosts = async () => {
+      if (!postsUuid) {
+        return;
+      }
+
       const accessToken = localStorage.getItem("access_token");
-      try {
-        const respost = await axios.get<Post>(
-          "https://honnaka-backend.azurewebsites.net/api/v1/post/d2b4b071-6930-4162-a5e7-b2296b987ddf",
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
+      if (!accessToken) {
+        window.location.href = "./signin";
+        return;
+      }
+
+      if (!ignore) {
+        const data = await Promise.all(
+          postsUuid.posts_uuid.map((post_uuid) => fetchPost(post_uuid))
         );
 
-        if (!ignore) {
-          setPost(respost.data);
-          console.log("post:", respost.data);
-        }
-      } catch (e) {
-        console.log(`Exception: ${e}`);
+        setPosts(data);
+        console.log("posts:", posts);
       }
     };
-    if (!accessToken) return;
 
-    getPost();
+    fetchPosts();
 
     return () => {
       ignore = true;
     };
-  }, [accessToken]);
+  }, [postsUuid]);
 
   useEffect(() => {
     const fetchMyPageData = async () => {
@@ -356,71 +173,29 @@ const MyPage = () => {
     return <div>Loading...</div>;
   }
 
-  const summary = post && post.summary;
-  const title = post && post.title;
-  const body = post && post.body;
-  const since = post && post.since;
-  const user_name = post && myUser.user_name;
-
   return (
+    <div>a</div>
     // <div>
     //   <Header />
     //   <div>
-    //     <h1>My Page</h1>
+    //     <h1>My page</h1>
+    //     <h2>username : {user_name}</h2>
     //     <div>
-    //       <h2>Username :{myUser.user_name}</h2>
-    //     </div>
-    //     <div>
-    //       <h2>My Posts</h2>
-    //       <ul>
-    //         <li>{title}</li>
-    //         <li>{summary}</li>
-    //         <li>{body}</li>
-    //         <li>{since}</li>
-    //         {/* {post.map((post) => ( */}
-    //         {/* <li key={post.title}>
-    //           <h3>{post.title}</h3>
-    //           <p>{post.content}</p>
-    //         </li> */}
-    //         {/* ))} */}
-    //       </ul>
+    //       <Card sx={{ maxWidth: 768 }}>
+    //         <CardContent>
+    //           <Typography sx={{ mb: 1.5 }} color="text.secondary">
+    //             {user_name}
+    //           </Typography>
+    //           <Typography variant="h5">{title}</Typography>
+    //           <Typography variant="body1">{summary}</Typography>
+    //           <Typography sx={{ mb: 1.5 }} color="text.secondary">
+    //             {since}
+    //           </Typography>
+    //         </CardContent>
+    //       </Card>
     //     </div>
     //   </div>
     // </div>
-    <div>
-      <Header />
-      <div>
-        <h1>My page</h1>
-        <h2>username : {user_name}</h2>
-        <div>
-          <Card sx={{ maxWidth: 768 }}>
-            <CardContent>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                {user_name}
-              </Typography>
-              <Typography variant="h5">{title}</Typography>
-              <Typography variant="body1">{summary}</Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                {since}
-              </Typography>
-            </CardContent>
-            {/* <CardActions>
-            <IconButton area-label="like" onClick={handle_like}>
-              {get_like(reaction?.like)}
-              {get_likes(reactions?.like)}
-            </IconButton>
-            <IconButton area-label="super_like" onClick={handle_super_like}>
-              {get_super_like(reaction?.super_like)}
-              {get_super_likes(reactions?.super_like)}
-            </IconButton>
-            <IconButton area-label="next" onClick={handle_next}>
-              <ArrowForwardIcon />
-            </IconButton>
-          </CardActions> */}
-          </Card>
-        </div>
-      </div>
-    </div>
   );
 };
 
